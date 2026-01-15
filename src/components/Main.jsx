41aeaffb94ac85307d34fa3af66635b760b519c5
kylemucha <kylemuchaswe@gmail.com>
2026-01-14 22:50:28 -0800

    Added the shape of the Tenzies game

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <>
+            <main>
+                <p>random girl:AP Lunch</p>
+                <p>Julia:HAHAHHAHAHAHHAHAHAHHA</p>
+            </main>
+        </>
+    )
+}
