b15c4ef593a0b60cd3fa6ed167666a591441ba02
kylemucha <kylemuchaswe@gmail.com>
2026-01-15 23:36:15 -0800

    some progress on the layout of the Tenzies game

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,8 @@ export default function Main() {
     return (
         <>
             <main>
-                <p>random girl:AP Lunch</p>
-                <p>Julia:HAHAHHAHAHAHHAHAHAHHA</p>
+                <p>random girl:AP</p>
+                <p>Julia:</p>
             </main>
         </>
     )
